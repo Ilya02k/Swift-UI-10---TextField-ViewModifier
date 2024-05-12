dc406d83404fe5756dd8f51ef1ba0d453703128c
Ilya Kazlou <ilya02k@gmail.com>
2024-05-12 13:12:32 +0300

    text field and focus

---
+++ Swift ui 10/Swift ui 10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift ui 10
+//
+//  Created by Ann Dosova on 12.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift ui 10/Swift ui 10/Swift_ui_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ui_10App.swift
+//  Swift ui 10
+//
+//  Created by Ann Dosova on 12.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ui_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
